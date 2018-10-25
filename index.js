20345de4fe827ee0a38222f4ff18ce1bb1958f92
ulleputtsepp <ulle.puttsepp@khk.ee>
2018-10-25 16:13:53 +0300

    asd

---
+++ index.js
@@ -0,0 +1,19 @@
+//ECHO is on.
+    var request = require('superagent');
+
+var getUserFollowers = function(username, callback) {
+    request
+        .get(`https://api.github.com/users/${username}/followers`)
+        .end(function(err, res) {
+            if (!err) {
+                var users = res.body.map(function(user) {
+                    return user.login;
+                });
+                callback(null, users);
+            } else {
+                callback('Error Occurred!');
+            }
+        });
+};
+
+module.exports.getUserFollowers = getUserFollowers;
